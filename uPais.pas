786a9b5c2466e4d6d48bc46f9c3eeb5ecebfb008
rafaelscappini <scappinirafael@gmail.com>
2019-09-17 15:57:49 -0300

    Update uPais.pas

--- uPais.pas
+++ uPais.pas
@@ -40,32 +40,32 @@ end;
 
 function Pai.getDDI: Integer;
 begin
-
+  result := ddi;
 end;
 
 function Pai.getNome: String;
 begin
-
+  result := nome;
 end;
 
 function Pai.getSigla: String;
 begin
-
+  result := sigla;
 end;
 
 procedure Pai.setDDI(vDDI: Integer);
 begin
-
+  self.ddi := vDDI;
 end;
 
 procedure Pai.setNome(vNome: String);
 begin
-
+  self.nome := vNome;
 end;
 
 procedure Pai.setSigla(vSigla: String);
 begin
-
+  self.sigla := vSigla;
 end;
 
 end.
