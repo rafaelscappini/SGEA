0508d863632a33073aa956c1194742ea908f9766
rafaelscappini <scappinirafael@gmail.com>
2019-09-17 16:47:31 +0100

    Create uPais.pas

---
+++ uPais.pas
@@ -0,0 +1,18 @@
+unit uPais;
+
+interface
+  uses
+    uPai;
+  type
+    Pais = class(Pai)
+  private
+    nome, sigla: String;
+    ddi: Integer;
+  public
+  
+  end;
+implementation
+
+{ Pais }
+
+end.
