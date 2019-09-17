04d41e896db4f232dacecc46aa70259933232cc3
rafaelscappini <scappinirafael@gmail.com>
2019-09-17 16:54:17 -0300

    Update uInter.pas

--- uInter.pas
+++ uInter.pas
@@ -1,1 +1,42 @@
 unit uInter;
+
+interface
+  uses
+    uController, uFrmConsultarPais, uPais;
+  type
+    Interfaces = class
+  private
+    umaController: Controller;
+    umFrmConsultarPais: TFrmConsultarPais;
+  public
+    constructor crieObjeto;
+    destructor destruaSe;
+    
+    procedure pesquisarPais(vPais: Pais);
+  end;
+implementation
+
+{ Interfaces }
+
+constructor Interfaces.crieObjeto;
+begin
+  self.umaController      := Controller.crieObjeto;
+  self.umFrmConsultarPais := TFrmConsultarPais.Create(nil);
+end;
+
+destructor Interfaces.destruaSe;
+begin
+  self.umFrmConsultarPais.FreeOnRelease;
+  
+  if Assigned(umaController) then
+    self.umaController.destruaSe;
+end;
+
+procedure Interfaces.pesquisarPais(vPais: Pais);
+begin
+  umFrmConsultarPais.Caption := 'Pesquisar País';
+  umFrmConsultarPais.conhecaObjeto(umaController, vPais);
+  umFrmConsultarPais.ShowModal;
+end;
+
+end.
