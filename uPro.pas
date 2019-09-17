13d238c8e8ef96aeec27c93b0229692fa8351a7e
rafaelscappini <scappinirafael@gmail.com>
2019-09-17 16:58:51 -0300

    Update uPro.pas

--- uPro.pas
+++ uPro.pas
@@ -1,1 +1,42 @@
 unit uPro;
+
+interface
+  uses
+    uFrmPrincipal, uInter, uPais;
+  type
+    Programa = class
+  private
+    umFrmPrincipal: TFrmPrincipal;
+    umaInter: Interfaces;
+    umPais: Pais;
+  public
+    constructor crieObjeto;
+    destructor destruaSe;
+    
+    procedure executeSe;
+  end;
+implementation
+
+{ Programa }
+
+constructor Programa.crieObjeto;
+begin
+  self.umFrmPrincipal := TFrmPrincipal.Create(nil);
+  self.umaInter       := Interfaces.crieObjeto;
+  self.umPais         := Pais.crieObjeto;
+end;
+
+destructor Programa.destruaSe;
+begin
+  self.umFrmPrincipal.FreeOnRelease;
+  self.umaInter.destruaSe;
+  self.umPais.destruaSe;
+end;
+
+procedure Programa.executeSe;
+begin
+  umFrmPrincipal.conhecaObjeto(umaInter, umPais);
+  umFrmPrincipal.ShowModal;
+end;
+
+end.
