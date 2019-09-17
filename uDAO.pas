840dfec333dc3f0d5435dacfe1bc054ad2494ee0
rafaelscappini <scappinirafael@gmail.com>
2019-09-17 16:03:45 -0300

    Update uDAO.pas

--- uDAO.pas
+++ uDAO.pas
@@ -1,1 +1,46 @@
+unit uDAO;
 
+interface
+  uses
+    uDM;
+  type
+    DAO = class
+  private
+    umDM: TDM;
+  public
+    constructor crieObjeto(vUmDM: TDM);
+    destructor destruaSe;
+    
+    function getUmDM: TDM;
+    
+    procedure setUmDM(vUmDM: TDM);
+  end;
+implementation
+
+{ DAO }
+
+constructor DAO.crieObjeto(vUmDM: TDM);
+begin
+  if Assigned(vUmDM) then
+    self.umDM := vUmDM
+  else
+    self.umDM := TDM.Create(nil);
+end;
+
+destructor DAO.destruaSe;
+begin
+  if Assigned(umDM) then
+    self.umDM.FreeOnRelease;
+end;
+
+function DAO.getUmDM: TDM;
+begin
+  result := umDM;
+end;
+
+procedure DAO.setUmDM(vUmDM: TDM);
+begin
+  self.umDM := vUmDM;
+end;
+
+end.
